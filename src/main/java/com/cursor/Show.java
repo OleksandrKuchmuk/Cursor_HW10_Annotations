ef952b1b61323d26f650dcf16459ea77a80fb263
Oleksandr Kuchmuk <96177236+OleksandrKuchmuk@users.noreply.github.com>
2022-09-01 19:26:20 +0300

    Create custom annotation

---
+++ src/main/java/com/cursor/Show.java
@@ -0,0 +1,9 @@
+package com.cursor;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Show {
+    boolean value() default true;
+}
