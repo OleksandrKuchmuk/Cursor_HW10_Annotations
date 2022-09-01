7de479d4b865a782f3242a3d28b8ec14ccd58425
Oleksandr Kuchmuk <96177236+OleksandrKuchmuk@users.noreply.github.com>
2022-09-01 19:32:54 +0300

    Create class Service and a method 'print' for processing task 1

---
+++ src/main/java/com/cursor/Service.java
@@ -0,0 +1,28 @@
+package com.cursor;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+public class Service {
+    public static final Logger LOGGER = LogManager.getLogger(Service.class);
+
+    public void print(Object o, Class c) {
+        Field[] fields = c.getDeclaredFields();
+        for (Field field : fields) {
+            Annotation[] annotations = field.getDeclaredAnnotations();
+            for (Annotation annotation : annotations) {
+                if (annotation.annotationType().equals(Show.class)) {
+                    field.setAccessible(true);
+                    try {
+                        LOGGER.info(field.get(o));
+                    } catch (IllegalAccessException e) {
+                        LOGGER.error("IllegalAccessException in print method");
+                    }
+                }
+            }
+        }
+    }
+}
